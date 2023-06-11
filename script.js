3d2f0c9ed09b9ed9b65cdc5bb427a4e19e8690f1
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2023-06-11 00:20:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(event) {
+    event.target.innerText = "Siga em frente!";
+}
+;
+function out(event) {
+    event.target.innerText = "Vamos lá!";
+}
---
+++ script.ts
@@ -0,0 +1,7 @@
+function hover(event:any){
+    event.target.innerText = "Siga em frente!";
+};
+
+function out(event:any){
+    event.target.innerText = "Vamos lá!"
+}
