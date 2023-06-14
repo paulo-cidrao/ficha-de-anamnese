497e71409de57583a2c717c517be1a4e14afd7aa
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2023-06-14 19:15:06 -0300

    FIX- Scripts de validação da pagina 3

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function hover(event) {
 function out(event) {
     event.target.innerText = "Vamos lá!";
 }
+function hover3(event) {
+    event.target.innerText = "Ir para proxima";
+}
+;
+function out3(event) {
+    event.target.innerText = "Pular";
+}
+/////////////////
+// validação da pagina 3//
+/////////////////
+var form = document.querySelector('.form');
+var radioButtons = Array.prototype.slice.call(document.querySelectorAll('.form input[type="radio"]'));
+var enivar = document.querySelector('.enviar');
+var error = document.querySelector('.valid');
+function pag3() {
+    var check = radioButtons.some(function (radioButton) { return radioButton.checked; });
+    if (!check) {
+        error.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + ' ' + "Por favor marque uma das opções acima!";
+    }
+    else {
+        window.location.href = "./pagina4.html";
+    }
+}
--- script.ts
+++ script.ts
@@ -1,7 +1,33 @@
-function hover(event:any){
+function hover(event: any) {
     event.target.innerText = "Siga em frente!";
 };
 
-function out(event:any){
+function out(event: any) {
     event.target.innerText = "Vamos lá!"
+}
+
+function hover3(event: any) {
+    event.target.innerText = "Ir para proxima";
+};
+
+function out3(event: any) {
+    event.target.innerText = "Pular"
+}
+/////////////////
+// validação da pagina 3//
+/////////////////
+const form =document.querySelector('.form');
+const radioButtons =Array.prototype.slice.call(document.querySelectorAll('.form input[type="radio"]'));
+const enivar = document.querySelector('.enviar');
+const error = document.querySelector('.valid')!;
+
+function pag3(){
+    const check = radioButtons.some((radioButton: any) => (radioButton as any as HTMLInputElement).checked);
+
+    if(!check){
+    error.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + ' ' + "Por favor marque uma das opções acima!";
+    }else{
+        window.location.href="./pagina4.html"
+    }
+
 }
