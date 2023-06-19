8cb35b802295341c07befa768065aeeed79ff568
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2023-06-18 21:55:05 -0300

    FIX-Validação da pagina 3

--- script.js
+++ script.js
@@ -17,14 +17,27 @@ function out3(event) {
 /////////////////
 var form = document.querySelector('.form');
 var radioButtons = Array.prototype.slice.call(document.querySelectorAll('.form input[type="radio"]'));
-var enivar = document.querySelector('.enviar');
 var error = document.querySelector('.valid');
+var select = document.querySelector('.select');
+var lista = document.querySelector(".inputs");
+var inputs = Array.prototype.slice.call(lista.getElementsByTagName("input"));
 function pag3() {
     var check = radioButtons.some(function (radioButton) { return radioButton.checked; });
-    if (!check) {
+    var valorSelecionado = select.value;
+    var listaDeValores = "";
+    inputs.forEach(function (input) {
+        if (input.checked) {
+            listaDeValores = input.value;
+        }
+    });
+    console.log(listaDeValores);
+    console.log(valorSelecionado);
+    if (!check || valorSelecionado == "") {
         error.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + ' ' + "Por favor marque uma das opções acima!";
     }
     else {
+        localStorage.setItem("uso_de_remedios", valorSelecionado);
+        localStorage.setItem("tipo_de_remedios", listaDeValores);
         window.location.href = "./pagina4.html";
     }
 }
