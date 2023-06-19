92740def4245ddc976f6d5a9d466fdee81dc6eeb
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2023-06-18 21:57:10 -0300

    FIX - bug do if

--- script.ts
+++ script.ts
@@ -26,8 +26,17 @@ const inputs = Array.prototype.slice.call(lista.getElementsByTagName("input")) a
 
 function pag3(){
     const check = radioButtons.some((radioButton: any) => (radioButton as any as HTMLInputElement).checked);
-
-    if(!check){
+    const valorSelecionado = select.value;
+    let listaDeValores:string = ""
+    inputs.forEach((input) => {
+        if (input.checked) {
+          listaDeValores = input.value;
+        }
+      });
+    console.log( listaDeValores)
+    console.log(valorSelecionado)
+    
+    if(!check || valorSelecionado == ""){
         error.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + ' ' + "Por favor marque uma das opções acima!";
     }else{
        localStorage.setItem("uso_de_remedios",valorSelecionado);
